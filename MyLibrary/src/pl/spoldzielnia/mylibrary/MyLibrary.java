73c2da80ae803dcfac819b9dd17a41b6cd590cfd
tptak <tomasz@ptak.net.pl>
2013-02-16 19:57:46 +0100

    Added default activity with some menu items

--- MyLibrary/src/pl/spoldzielnia/mylibrary/MyLibrary.java
+++ MyLibrary/src/pl/spoldzielnia/mylibrary/MyLibrary.java
@@ -3,22 +3,26 @@ package pl.spoldzielnia.mylibrary;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
 public class MyLibrary extends ListActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_library);
-        
-    }
+	private static String[] menuOptions = { "Lent items", "People", "All items" };
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my_library);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my_library, menu);
-        return true;
-    }
-    
+		setListAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, menuOptions));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.my_library, menu);
+		return true;
+	}
+
 }
