4500fa1f77e0fe5956429a9aa8337e2f050e32f4
tptak <tomasz@ptak.net.pl>
2013-03-20 21:34:20 +0100

    First Fragments attempt - a class for MainActivity

---
+++ MyLibrary/src/pl/spoldzielnia/mylibrary/fragments/MainFragment.java
@@ -0,0 +1,17 @@
+package pl.spoldzielnia.mylibrary.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+}
